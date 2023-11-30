332434936cc952735b7ffc6e6cf3ff65f11ad423
George Stagg <george.stagg@posit.co>
2023-11-30 14:01:18 +0000

    Add webR dist dir to output file tracing
    Also updates webR to 0.2.2

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    outputFileTracingIncludes: {
+      '/': ['./node_modules/**/webr/*'],
+    },
+  },
 }
 
 module.exports = nextConfig
